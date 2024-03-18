78416eb5ebce45d19dbf2f9161ef181e44e8361d
Freedom-21 <yenesewfreedom8@gmail.com>
2024-03-18 21:49:29 +0100

    updated return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
 
     if length == 0:
         return length, None
     else:
-        return length, first_char
+        return length, sentence[0]
