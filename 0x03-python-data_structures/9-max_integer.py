c176ced89c5e35bbb571c7161c7a7733cbb34fa6
Freedom-21 <yenesewfreedom8@gmail.com>
2024-03-18 22:02:24 +0100

    find max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        return None
+
+    maximum = 0
+    for i in range(0, len(my_list)):
+        if my_list[i] > maximum:
+            maximum = my_list[i]
+
+    return maximum
