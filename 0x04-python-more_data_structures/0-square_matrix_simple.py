d51cb000e909da7f4805beb64756b9cceec2c2cb
Freedom-21 <yenesewfreedom8@gmail.com>
2024-03-20 23:56:22 +0100

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    sqr_mat = []
+    for row in matrix:
+        sqr_row = []
+        for element in row:
+            sqr_row.append(element * 2)
+        sqr_mat.append(sqr_row)
+    return sqr_mat
