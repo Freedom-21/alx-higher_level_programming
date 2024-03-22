1e4cf32c07b3b85b270523a29ec20f832dd7bc99
Freedom-21 <yenesewfreedom8@gmail.com>
2024-03-22 15:57:18 +0100

    best scorer

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_score = 0
+    best_scorer = None
+    if a_dictionary is not None:
+        for k, v in a_dictionary.items():
+            if v > best_score:
+                best_score = v
+                best_scorer = k
+    return  best_scorer
