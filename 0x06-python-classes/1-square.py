36edf5f8d104858aee9e921b93eb50a9e81a0ad6
Freedom-21 <yenesewfreedom8@gmail.com>
2024-04-03 23:28:14 +0200

    square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+"""
+
+
+class Square:
+    """
+    Defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
