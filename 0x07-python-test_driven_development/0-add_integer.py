f933d9180d2c59f2ca32b587b54401829a4acb72
Freedom-21 <yenesewfreedom8@gmail.com>
2024-04-12 23:00:25 +0200

    add int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module provides utility functions for arithmetic operations.
+Currently, it contains a function to add two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Add two integers or floats.
+    The function casts floats into integers before addition.
+    
+    Args:
+    a (int, float): The first number, must be an integer or float.
+    b (int, float, optional): The second number, must be an integer or float. Defaults to 98.
+
+    Returns:
+    int: The sum of a and b after casting them to integers.
+
+    Raises:
+    TypeError: If either a or b is neither an integer nor a float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
