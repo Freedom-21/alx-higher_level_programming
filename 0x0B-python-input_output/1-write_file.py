28e202a961bdc3b30c6d47d92ca5d74a759c668b
Freedom-21 <yenesewfreedom8@gmail.com>
2024-04-16 23:41:29 +0200

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+A Module that wirtes into a file
+"""
 def write_file(filename="", text=""):
     """
      Writes a text into a file
@@ -9,5 +11,5 @@ def write_file(filename="", text=""):
          text (str): a text to be written to the filename
    
     """
-    with open(filename, "w", encoding="UTF8") as f:
-        print(f.write(text))
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
