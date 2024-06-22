f202ca7c33292ef2a4f1ea73c53d8652ed29e848
Freedom-21 <yenesewfreedom8@gmail.com>
2024-06-22 17:08:38 +0200

    ORM updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 
 
-if __name__ == "__main__":
-    # Connecting to the MySQL DB
+def list_states(username, password, database_name):
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        username=argv[1],
-        password=argv[2],
-        db_name=argv[3]
+        user=username,
+        passwd=password,
+        db=database_name
     )
+
+    # Create a cursor object to interact with the database
     cur = db.cursor()
+
+    # Execute the SQL query to select all states
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    # Fetch all the results of the query
     rows = cur.fetchall()
+
+    # Print each row
     for row in rows:
         print(row)
 
+    # Close the cursor and database connection
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # List all states
+    list_states(username, password, database_name)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python3
 import MySQLdb
+import sys
+
+
+def filter_states(username, password, database_name):
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query to select states with names starting with 'N'
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the results of the query
+    rows = cur.fetchall()
+
+    # Print each row
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+#!/usr/bin/python3
+import MySQLdb
 from sys import argv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Connecting to the MySQL DB
     db = MySQLdb.connect(
         host="localhost",
@@ -13,12 +44,20 @@ if __name__ == "__main__":
         db_name=argv[3]
     )
     cur = db.cursor()
-    cur.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    )
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # List all states with names starting with 'N'
+    filter_states(username, password, database_name)
