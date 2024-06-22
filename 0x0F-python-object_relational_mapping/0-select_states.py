8c370b6f18139fc814aad2924480292c64037ab8
Freedom-21 <yenesewfreedom8@gmail.com>
2024-06-22 17:12:28 +0200

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
+"""Lists states"""
+
 import MySQLdb
-import sys
-
-
-def list_states(username, password, database_name):
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database_name
-    )
-
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
-
-    # Execute the SQL query to select all states
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-
-    # Fetch all the results of the query
-    rows = cur.fetchall()
-
-    # Print each row
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cur.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    # Get the command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # List all states
-    list_states(username, password, database_name)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
