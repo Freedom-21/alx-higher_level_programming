a08edc498fbb5bf241ca783c688ee8a69ccb3e01
Freedom-21 <yenesewfreedom8@gmail.com>
2024-06-22 23:48:27 +0200

    multi strings

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Print 3 lines
+ */
+
+const myVar0 = 'C is fun';
+console.log(myVar0);
+const myVar1 = 'Python is cool';
+console.log(myVar1);
+const myVar2 = 'JavaScript is amazing';
+console.log(myVar2);
