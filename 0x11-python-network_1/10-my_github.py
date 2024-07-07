e2b5f22d03e7f4bc3fda6401dda58a8113be63e4
Freedom-21 <yenesewfreedom8@gmail.com>
2024-07-07 16:29:01 +0200

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+username = sys.argv[1]
+token = sys.argv[2]
+url = 'https://api.github.com/user'
+response = requests.get(url, auth=(username, token))
+print(response.json().get('id'))
